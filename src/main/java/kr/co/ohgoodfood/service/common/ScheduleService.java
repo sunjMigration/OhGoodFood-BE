eb4aaf7206f2b6fda15dc831814c4065a5009d89
sunJ0120 <sspure1214@gmail.com>
2025-12-14 11:09:24 +0900

    refactor: springboot 기반으로 변경을 위해 파일 구조 정리 및 build.gradle 설정 완료

--- src/main/java/kr/co/ohgoodfood/service/common/ScheduleService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.ohgoodfood.service.common;
-
-public interface ScheduleService {
-    public void reservationCheck();
-    public void reservationCheckBeforeOneHour();
-    public void pickupCheck();
-    public void pickupStartCheck();
-}
